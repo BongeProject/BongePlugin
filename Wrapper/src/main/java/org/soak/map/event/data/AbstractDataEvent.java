a250bbc39bb5b8930be5fcc09ab7ba644337ff0b
mosemister <mosemister@users.noreply.github.com>
2025-02-17 12:15:22 +0000

    Added air event

--- Wrapper/src/main/java/org/soak/map/event/data/AbstractDataEvent.java
+++ Wrapper/src/main/java/org/soak/map/event/data/AbstractDataEvent.java
@@ -1,4 +1,4 @@
-package org.soak.map.event.entity.player.data;
+package org.soak.map.event.data;
 
 import org.bukkit.event.Event;
 import org.bukkit.event.EventPriority;
@@ -38,6 +38,10 @@ public abstract class AbstractDataEvent<T, BE extends Event> extends SoakEvent<C
         fireEvent(event, holder, changedTo, original);
     }
 
+    @Override
+    protected Class<ChangeDataHolderEvent.ValueChange> spongeEventClass() {
+        return ChangeDataHolderEvent.ValueChange.class;
+    }
 
     protected abstract void fireEvent(ChangeDataHolderEvent.ValueChange spongeEvent, DataHolder.Mutable player,
                                       T changedTo, T changedFrom);
